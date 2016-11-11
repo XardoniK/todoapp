ef1e0eba0a50bd97b71b4f7126aad9269e295235
xardonik <xardonik@gmail.com>
2016-11-11 21:13:07 +0100

    prawie gotowa appka, wiekzsość działa, czekam na feedback

--- js/app.js
+++ js/app.js
@@ -1,59 +1,102 @@
 /**
  * Created by xardonik on 21.10.2016.
  */
-var app = angular.module('ToDo', []);
+var app = angular.module('toDo', [
+    'ngResource'
+]);
 
-app.controller('ListsCtrl', function ($scope, $rootScope, $http, $templateCache) {
-
-    $rootScope.newName = "";
-
-    $scope.data = {
-        "lists": [
-            {
-                "id": 1,
-                "title": "Zakupy",
-                "tasks": [
-                    {
-                        "name": "Banany",
-                    },
-                    {
-                        "name": "Cukier",
-                    },
-                    {
-                        "name": "Chleb",
-                    },
-                    {
-                        "name": "Dupa Macieja",
-                    }
-                ],
+app.factory("TaskService", function ($resource) {
+    return $resource(
+        "webservices/web/app_dev.php/api/task/:action",
+        {
+            action: '@action',
+            user_id: 2
+        },
+        {
+            getTasks: {
+                method: 'GET',
+                params: {
+                    action: 'get'
+                },
+                isArray: true
             },
-        ],
-    };
-
-    $scope.TaskRemove = function (task_id) {
-        $('#task_' + task_id).remove();
-        $scope.data.lists[0].tasks.splice(task_id, 1);
-    };
-
-    $scope.ListRemove = function (list_id) {
-        $('#list_' + list_id).remove();
-    };
-
-    $scope.AddTask = function () {
-        if($rootScope.newName != "") {
-            newNamesArray = $rootScope.newName.split(";");
-            if (newNamesArray.length > 1) {
-                for (i = 0; i < newNamesArray.length; i++) {
-                    $scope.data.lists[0].tasks.push({
-                        "name": newNamesArray[i]
-                    });
-                }
-            } else {
-                $scope.data.lists[0].tasks.push({
-                    "name": $rootScope.newName
-                });
+            createTask: {
+                method: 'POST',
+                params: {
+                    action: 'create',
+                    name: '@name'
+                },
+                isArray: false
+            },
+            modifyTask: {
+                method: 'POST',
+                params: {
+                    action: 'modify',
+                    task_id: '@task_id',
+                    name: '@name'
+                },
+            },
+            removeTask: {
+                method: 'DELETE',
+                params: {
+                    action: 'delete',
+                    task_id: '@task_id'
+                },
+                isArray: false
             }
-            $rootScope.newName = "";
         }
-    }
+    );
+});
+
+app.controller('ListsCtrl', function ($scope, $rootScope, TaskService) {
+
+    $rootScope.createdTaskName = "";
+    $scope.editTasks = true;
+
+    $scope.data = TaskService.getTasks();
+
+    $scope.modifyTask = function (index, id, element) {
+
+        if ($scope.backupElement && $scope.backupElement != element) {
+            $scope.backupElement.editingTask = false;
+            $scope.backupElement.task.name = $scope.backupName;
+        }
+
+        if (element.editingTask) {
+            element.editingTask = false;
+            if ($scope.data[index].name.trim() == "" || $scope.backupName == $scope.data[index].name.trim()) {
+                $scope.data[index].name = $scope.backupName;
+            } else if($scope.data[index].name.trim() != ""){
+                TaskService.modifyTask({task_id: id, name: $scope.data[index].name});
+            }
+            $scope.backupElement = "";
+            $scope.backupName = "";
+        } else {
+            element.editingTask = true;
+            $scope.backupName = $scope.data[index].name;
+            $scope.backupElement = element;
+            $("#name_" + index).focus();
+
+        }
+
+    };
+
+    $scope.createTask = function () {
+        if ($rootScope.createdTaskName.trim() != "") {
+            TaskService.createTask({name: $rootScope.createdTaskName}, function (data) {
+                $scope.data.push({
+                    id: data.id,
+                    name: $rootScope.createdTaskName
+                });
+                $rootScope.createdTaskName = "";
+            });
+        }
+
+    };
+
+    $scope.removeTask = function (index, id) {
+        TaskService.removeTask({task_id: id});
+        $scope.data.splice(index, 1);
+
+    };
 });
